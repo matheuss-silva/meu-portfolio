a98da4c8fb50c3f026097a46f4cc3b592b2e44ab
Matheus Silva <57456752+matheuss-silva@users.noreply.github.com>
2026-04-06 09:56:09 -0300

    up project

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -8,32 +8,60 @@ export const projects = [
     link: "https://play.google.com/store/apps/details?id=petmeup.app.cliente&hl=pt_BR",
     subtitle: "Flutter, Dart, Firebase, API REST",
   },
-    {
-    title: "PetMe-Up|Parceiro",
+  {
+    title: "Senac TO",
     description:
-      "O aplicativo PetMe-Up Parceiros é a ferramenta ideal para facilitar a gestão do seu negócio dentro da plataforma PetMe-Up. Integrado ao Painel Administrativo.",
+      "Portal de acesso do Senac TO para o Espaço do Aluno e serviços institucionais.",
     image:
-      "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080752/download_capqln.webp",
-    link: "https://play.google.com/store/apps/details?id=dix.digital.fyimoveis&hl=pt_BR",
-    subtitle: "Flutter, Dart, Firebase, API REST",
+      "https://res.cloudinary.com/dho0yyj47/image/upload/v1775479618/Captura_de_tela_2026-04-06_094650_llgaif.png",
+    link: "https://d-senacto.dixid.digital/",
+    subtitle: "Vue.js, JavaScript, Node.js, API REST",
   },
-    {
+
+  {
     title: "Lanche Bem",
     description:
-      "A Lanche Bem foi criada para ajudar as crianças e os pais na alimentação escolar, gerando intervalos mais rápidos e com menos filas, educação financeira e uma alimentação mais equilibrada.",
+      "Plataforma que facilita a alimentação escolar com mais agilidade, organização e menos filas.",
     image:
       "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080805/unnamed_lbkqxl.webp",
     link: "https://play.google.com/store/apps/details?id=dix.digital.lanchebem&hl=pt_BR",
     subtitle: "Flutter, Dart, Firebase, API REST",
   },
-    {
-    title: "Senac TO",
+
+  {
+    title: "PetMe-Up|Parceiro",
     description:
-      "Com o Agente, você pode compartilhar vídeos curtos que destacam suas habilidades esportivas e interagir com uma comunidade de atletas.",
+      "Aplicativo para parceiros gerenciarem serviços, pedidos e operações dentro da plataforma PetMe-Up.",
     image:
-      "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080954/Captura_de_tela_2025-09-05_104139_rm8kj3.png",
-    link: "https://play.google.com/store/apps/details?id=dix.digital.oagente&hl=pt_BR",
+      "https://res.cloudinary.com/dho0yyj47/image/upload/v1757079397/logoNew_jjhqdz.jpg",
+    link: "https://play.google.com/store/apps/details?id=petmeup.app.parceiro&hl=pt",
+    subtitle: "Flutter, Dart, Firebase, API REST",
+  },
+  {
+    title: "Radical",
+    description:
+      "Oferecendo experiências premium de rally e aventura off-road para quem busca emoção, sofisticação e momentos inesquecíveis.",
+    image:
+      "https://res.cloudinary.com/dho0yyj47/image/upload/v1775479789/Captura_de_tela_2026-04-06_094940_vndilj.png",
+    link: "https://d-piocera.dixid.digital/",
     subtitle: "Vue.js, JavaScript, Node.js, API REST",
+  },
+  {
+    title: "Netcare",
+    description:
+      "Em um mundo onde o acesso à saúde e aos serviços financeiros é muitas vezes desigual, a Netcare busca eliminar essa disparidade, oferecendo uma plataforma inclusiva e acessível a todos.",
+    image:
+      "https://res.cloudinary.com/dho0yyj47/image/upload/v1775479381/Captura_de_tela_2026-04-06_094252_e4jkx2.png",
+    link: "https://play.google.com/store/apps/details?id=dix.com.netcare.app",
+    subtitle: "Flutter, Dart, Firebase, API REST",
+  },
+  {
+    title: "CP Recomenda",
+    description: "Aplicativo exclusivo para vendedores da Casas Pedro.",
+    image:
+      "https://res.cloudinary.com/dho0yyj47/image/upload/v1775479502/Captura_de_tela_2026-04-06_094453_luylsw.png",
+    link: "https://play.google.com/store/apps/details?id=dix.digital.casaspedro.offers",
+    subtitle: "Flutter, Dart, Firebase, API REST",
   },
   {
     title: "Agente",
@@ -44,33 +72,15 @@ export const projects = [
     link: "https://play.google.com/store/apps/details?id=dix.digital.oagente&hl=pt_BR",
     subtitle: "Flutter, Dart, Firebase, API REST",
   },
-    {
-    title: "Radical",
-    description:
-      "A PetMe-Up foi desenvolvida para simplificar a entrega de produtos e serviços do segmento Pet. Através do aplicativo, você, Pai de Pet, têm acesso a tudo em um só lugar, podendo realizar pedidos e/ou agendamento de serviços.",
-    image:
-      "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080839/Captura_de_tela_2025-09-05_104748_nra1yp.png",
-    link: "https://play.google.com/store/apps/details?id=petmeup.app.cliente&hl=pt_BR",
-    subtitle: "Vue.js, JavaScript, Node.js, API REST",
-  },
-    {
-    title: "Netcare",
-    description:
-      "Em um mundo onde o acesso à saúde e aos serviços financeiros é muitas vezes desigual, a Netcare busca eliminar essa disparidade, oferecendo uma plataforma inclusiva e acessível a todos.",
-    image:
-      "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080805/unnamed_lbkqxl.webp",
-    link: "https://play.google.com/store/apps/details?id=dix.com.netcare.app",
-    subtitle: "Flutter, Dart, Firebase, API REST",
-  },
-    {
+  {
     title: "Contratei MEI APP",
     description:
       "CONTRATEI, uma plataforma de rápida solução e contratação de MEIs e Autônomos prestadores de serviço, com toda segurança que um aplicativo pode oferecer, com um sistema único de contratação interna e avaliação.",
     image:
-      "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080752/download_capqln.webp",
+      "https://res.cloudinary.com/dho0yyj47/image/upload/v1775479426/Captura_de_tela_2026-04-06_094336_mbpve5.png",
     link: "https://play.google.com/store/apps/details?id=dix.digital.contratei.app",
     subtitle: "Flutter, Dart, Firebase, API REST",
-  }, 
+  },
   {
     title: "Fy Imóveis",
     description:
@@ -79,15 +89,5 @@ export const projects = [
       "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080752/download_capqln.webp",
     link: "https://play.google.com/store/apps/details?id=dix.digital.fyimoveis&hl=pt_BR",
     subtitle: "Flutter, Dart, Firebase, API REST",
-  }, 
-  {
-    title: "CP Recomenda",
-    description:
-      "Aplicativo exclusivo para vendedores da Casas Pedro.",
-    image:
-      "https://res.cloudinary.com/dho0yyj47/image/upload/v1757080954/Captura_de_tela_2025-09-05_104139_rm8kj3.png",
-    link: "https://play.google.com/store/apps/details?id=dix.digital.casaspedro.offers",
-    subtitle: "Flutter, Dart, Firebase, API REST",
   },
 ];
-
